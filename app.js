53505bfb6483325e2e81839cf5c428f0a42dbfa6
KashishKosalge <121129712+KashishKosalge@users.noreply.github.com>
2023-12-13 23:18:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function(){
+    // console.log("generate random color");
+    let h2 = document.querySelector("h2");
+    let randomColor = getRandomColor();
+    h2.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+
+    // console.log("Color updated!");
+
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
